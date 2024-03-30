e8432f7bd2071aa21f0773d0a4c702ef73888c30
Umarjon <urr777777@gmail.com>
2024-03-30 14:43:41 +0500

    FUNCTION HAS BEEN DONE

---
+++ index.js
@@ -0,0 +1,41 @@
+ let guestList = "Putin  Biden  Zelenskiy Lukashenko Mirziyoyev Makaron Erdogan"
+let presidentsname = prompt('What is your name mr president?') 
+
+ if(presidentsname===guestList) {
+    alert("come")
+ }else {
+    alert("goodbye")
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let num = prompt('any num')
+
+
+let names= "a"
+let name = prompt(`What is your name?`) 
+if(name!==names) {
+    alert("come")
+}else {
+    alert("goodday")
+}
+
+
+
+
+
+
+
+
+
